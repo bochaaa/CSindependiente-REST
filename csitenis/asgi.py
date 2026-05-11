86d165decb5c3a9f5eca47260552f6dea4b9f7c9
bochaaa <bochaa1247@gmail.com>
2026-05-11 11:05:07 -0300

    Initial Django REST project for CSI Tenis

---
+++ csitenis/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for csitenis project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csitenis.settings')
+
+application = get_asgi_application()
